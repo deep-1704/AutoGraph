519013fe520b876c45bb1fbb4a4c096dd3a32534
Deep <dp124551634@gmail.com>
2024-01-09 20:26:10 +0530

    Add algorithm BFS - single source shortest paths

--- lib/src/main/java/autograph/Algorithm/BFS.java
+++ lib/src/main/java/autograph/Algorithm/BFS.java
@@ -3,12 +3,35 @@ package autograph.Algorithm;
 import autograph.Graph;
 import autograph.Util.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BFS {
     public static List<Integer> getShortestPathsFromSource(Graph graph, int src){
+        List<List<Pair<Integer,Integer>>> adjacencyList = graph.getAdjacencyList();
+        int n = adjacencyList.size();
 
-        return null;
+//        Initialize result
+        List<Integer> shortestPath = new ArrayList<>();
+        for(int i = 0;i<n;i++) shortestPath.add(-1);
+        shortestPath.set(src,0);
+
+//        Algorithm
+        Boolean[] visited = new Boolean[n];
+        Arrays.fill(visited, false);
+        visited[src] = true;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        while(!(q.isEmpty())){
+            int s = q.remove();
+            for(Pair<Integer,Integer> nei:adjacencyList.get(s)){
+                if(!visited[nei.first]){
+                    visited[nei.first] = true;
+                    shortestPath.set(nei.first, shortestPath.get(s)+1);
+                    q.add(nei.first);
+                }
+            }
+        }
+        return shortestPath;
     }
 }
---
+++ lib/src/test/java/autograph/BFSTest.java
@@ -0,0 +1,34 @@
+package autograph;
+
+import autograph.Algorithm.BFS;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class BFSTest {
+
+    @Test
+    void shouldProvideCorrectShortestPaths(){
+//        TC-01
+        Graph graph = new Graph(8,false);
+        graph.addEdge(0,1);
+        graph.addEdge(0,3);
+        graph.addEdge(1,2);
+        graph.addEdge(3,4);
+        graph.addEdge(3,7);
+        graph.addEdge(4,5);
+        graph.addEdge(4,6);
+        graph.addEdge(4,7);
+        graph.addEdge(5,6);
+        graph.addEdge(6,7);
+
+        List<Integer> sp = BFS.getShortestPathsFromSource(graph,0);
+        assertThat(sp.get(7)).isEqualTo(2);
+        assertThat(sp.get(1)).isEqualTo(1);
+        assertThat(sp.get(2)).isEqualTo(2);
+        assertThat(sp.get(3)).isEqualTo(1);
+        assertThat(sp.get(4)).isEqualTo(2);
+    }
+
+}
